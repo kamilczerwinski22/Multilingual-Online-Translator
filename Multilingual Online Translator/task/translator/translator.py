ec561cec2bbfdcc5fec12cd9e9d102d3280357e3
Kamil Czerwinski <kamilcze31@gmail.com>
2020-12-06 20:52:36 +0100

    Stage 1/7, first input

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -1,1 +1,14 @@
-print('Hello, World!')
+def show_message():
+    print('Type "en" if you want to translate from French into English, or "fr" if you want to translate from English into French:')
+
+
+
+def main():
+    show_message()
+    user_input_language = input()
+    print("Type the word you want to translate:")
+    user_input_text = input()
+    print(f'You chose "{user_input_language}" as the language to translate "{user_input_text}" to.')
+
+if __name__ == '__main__':  # run as script
+    main()
